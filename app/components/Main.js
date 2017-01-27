5709a99665e69ea815a85189a9a339b2479a4d2f
aileenvl <aileenvl@gmail.com>
2017-01-26 18:07:23 -0600

    react route

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,13 @@
 var React = require('react');
 
 var Main = React.createClass({
-	render: function(){
-		<div> Hello from Main! </div>
-	}
-})
+  render: function () {
+    return (
+      <div className='main-container'>
+        {this.props.children}
+      </div>
+    )
+  }
+});
 
-module.export Main;
+module.exports = Main;
